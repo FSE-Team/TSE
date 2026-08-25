f8d2ff085f42550f73d965503c6b0d4a7384cd2a
WorldEaster <WorldEasterOfficial@outlook.com>
2026-08-25 18:08:25 +0800

    修改了tooltip.js中的事件名称 更新了morecreateburnerswithemberbruner

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -1,4 +1,4 @@
-ItemEvents.tooltip(event => {
+ItemEvents.modifyTooltips(event => {
     const MODID = "tse:"
     event.add(MODID + "copper_coin", [Text.translate("tooltip.tse.copper_coin0"),Text.translate("tooltip.tse.copper_coin")])
     event.add(MODID + "compressed_iron_coin", [Text.translate("tooltip.tse.compressed_iron_coin0"),Text.translate("tooltip.tse.compressed_iron_coin")])
