07b6ed8f2a39237976074cb4cac47000ba31b835
WorldEaster <WorldEasterOfficial@outlook.com>
2026-08-25 15:53:49 +0800

    删除了一些不必要的文件

--- kubejs/startup_scripts/block_modification.js
+++
@@ -1,8 +0,0 @@
-BlockEvents.modification(event => {
-    ////const woods = ["oak", "birch", "acacia", "cherry", "jungle", "spruce", "dark_oak", "mangrove"]
-    ////for (var i = 0; i < 8; i++) {
-    ////    event.modify(woods[i] + "_log",block=>{
-    ////    block.requiresTool = true
-    ////})
-    ////}
-})
--- kubejs/startup_scripts/machine_registry.js
+++
@@ -1,8 +0,0 @@
-////StartupEvents.registry("block", event => {
-////    const MODID = "tse:"
-////    event.create(MODID + "drying_rack", "custommachinery")
-////    event.create(MODID + "solar_reforge_pot", "custommachinery")
-////    event.create(MODID + "primitive_blast_furnace_controller", "custommachinery")
-////    event.create(MODID + "blast_furnace_controller", "custommachinery")
-////    event.create(MODID + "advanced_blast_furnace_controller", "custommachinery")
-////})
